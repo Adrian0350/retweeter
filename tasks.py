57f614514041e99e73ccbccef9ffb640a2ec911d
Gervasio Marchand <gmc@gmc.uy>
2016-10-26 11:35:58 -0300

    Handing username and icon on Slack (now that we're not using the user's avatar on twitter, it makes more sense)

--- tasks.py
+++ tasks.py
@@ -16,7 +16,5 @@ def retweet(tweet_id):
 @app.task
 def post_to_slack(tweet):
     requests.post(settings['slack_url'], json={
-        'text': '<https://twitter.com/%s/status/%s|New tweet> using the hashtag' % (tweet['user']['screen_name'], tweet['id_str']),
-        'username': '#PhillyCheeseStack @ Twitter',
-        'icon_url': 'https://pbs.twimg.com/profile_images/789819457711509505/KqkPyjP4.jpg'
+        'text': '<https://twitter.com/%s/status/%s|New tweet> using the hashtag' % (tweet['user']['screen_name'], tweet['id_str'])
     })
